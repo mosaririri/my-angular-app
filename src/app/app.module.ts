605d7d2f6571071d2cf9551b07f59057d1ce892f
mosaririri <dos.quoi813@gmail.com>
2019-12-16 01:15:20 +0900

    implement cart and shipping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
+/*
+  全体で利用できるインポートと機能
+*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     // routing
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailComponent}
+      { path: 'products/:productId', component: ProductDetailComponent},
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -24,7 +34,9 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [ AppComponent ]
 })
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    productId: 0,
+    productId: 1,
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
-    productId: 1,
+    productId: 2,
     name: 'Phone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    productId: 2,
+    productId: 3,
     name: 'Phone Standard',
     price: 299,
     description: ''
